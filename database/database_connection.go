a232bef9bf7ee72c23420e5169842e8bdc8dea2b
flakrim <flakrim.jusufi@gmail.com>
2021-06-30 13:39:54 +0200

    Added .env file

--- database/database_connection.go
+++ database/database_connection.go
@@ -1,13 +1,28 @@
 package database
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func Connect() *gorm.DB {
 
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=kimi dbname=testdb password=213630")
+	e := godotenv.Load() //Load .env file
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+
+	db, err := gorm.Open("postgres", dbUri)
 	if err != nil {
 		panic("Could not connect to db")
 	}
